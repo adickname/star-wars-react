c1d71a673893119ba1418ca4c1d0844f3c3dbe71
Adrian Pasterka <you@example.com>
2023-06-03 13:02:37 +0200

    Remove favicon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
 const Home = () => {
-    return <div>Home page</div>
+    return (
+        <section>
+            <article>
+                <p>
+                    Welcome, to a site dedicated to Star Wars. At this moment on
+                    the site you will find information about several movies,
+                    planets, characters and starships. The site will continue to
+                    grow. Eventually, the site should have more than a dozen
+                    planets, characters and starships, as well as a comment
+                    system. The site uses the Star Wars API https://swapi.dev/.
+                    Thank you for visiting.
+                </p>
+            </article>
+        </section>
+    )
 }
 
 export default Home
